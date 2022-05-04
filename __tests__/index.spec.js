31a28f3b52b89f26e092c2911738676e68669c62
emukupa1 <edward-manda@uhatok.com>
2022-05-04 08:47:06 -0500

    put placeholder describes, start test then with one test and ph

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,9 +1,29 @@
-const MyPromise = require("../src/my-promise");
-// const MyPromise = Promise;
+// const MyPromise = require("../src/my-promise");
+const MyPromise = Promise;
 
-console.log(MyPromise);
+const DEFAULT_VALUE = "default";
+
+const promise = ({ value = DEFAULT_VALUE, fail = false } = {}) =>
+  new MyPromise((resolve, reject) => {
+    fail ? reject(value) : resolve(value);
+  });
+
 describe("Promise Class", () => {
   it("Exist", () => {
     expect(typeof MyPromise).toEqual("function");
   });
 });
+
+describe("then", () => {
+  it("with no chaining", () =>
+    promise().then((v) => expect(v).toEqual(DEFAULT_VALUE)));
+  it.skip("with multiple thens for same promise", () =>
+    expect(true).toBe(false));
+  it.skip("with then and catch", () => expect(true).toBe(false));
+  it.skip("with chaining", () => expect(true).toBe(false));
+});
+describe.skip("catch", () => {});
+describe.skip("finally", () => {});
+describe.skip("all", () => {});
+describe.skip("race", () => {});
+describe.skip("any", () => {});
