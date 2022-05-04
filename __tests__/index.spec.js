703ace3b0c12b8e979d29a383cf14952e28f0db7
emukupa1 <edward-manda@uhatok.com>
2022-05-04 11:51:32 -0500

    test static methods

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -80,6 +80,72 @@ describe("finally", () => {
     return Promise.allSettled([successPromise, failPromise]);
   });
 });
-describe.skip("all", () => {});
-describe.skip("race", () => {});
-describe.skip("any", () => {});
+
+describe("static methods", () => {
+  it("resolve", () => {
+    return MyPromise.resolve(DEFAULT_VALUE).then((v) =>
+      expect(v).toEqual(DEFAULT_VALUE)
+    );
+  });
+
+  it("reject", () => {
+    return MyPromise.reject(DEFAULT_VALUE).catch((v) =>
+      expect(v).toEqual(DEFAULT_VALUE)
+    );
+  });
+
+  describe("all", () => {
+    it("with success", () => {
+      return MyPromise.all([promise({ value: 1 }), promise({ value: 2 })]).then(
+        (v) => expect(v).toEqual([1, 2])
+      );
+    });
+
+    it("with fail", () => {
+      return MyPromise.all([promise(), promise({ fail: true })]).catch((v) =>
+        expect(v).toEqual(DEFAULT_VALUE)
+      );
+    });
+  });
+
+  it("allSettled", () => {
+    return MyPromise.allSettled([promise(), promise({ fail: true })]).then(
+      (v) =>
+        expect(v).toEqual([
+          { status: "fulfilled", value: DEFAULT_VALUE },
+          { status: "rejected", reason: DEFAULT_VALUE },
+        ])
+    );
+  });
+
+  describe("race", () => {
+    it("with success", () => {
+      return MyPromise.race([
+        promise({ value: 1 }),
+        promise({ value: 2 }),
+      ]).then((v) => expect(v).toEqual(1));
+    });
+
+    it("with fail", () => {
+      return MyPromise.race([
+        promise({ fail: true, value: 1 }),
+        promise({ fail: true, value: 2 }),
+      ]).catch((v) => expect(v).toEqual(1));
+    });
+  });
+
+  describe("any", () => {
+    it("with success", () => {
+      return MyPromise.any([promise({ value: 1 }), promise({ value: 2 })]).then(
+        (v) => expect(v).toEqual(1)
+      );
+    });
+
+    it("with fail", () => {
+      return MyPromise.any([
+        promise({ fail: true, value: 1 }),
+        promise({ value: 2 }),
+      ]).catch((e) => expect(e.errors).toEqual([1, 2]));
+    });
+  });
+});
